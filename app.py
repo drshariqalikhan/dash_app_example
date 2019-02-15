687a33a2070b2e798368407f1494188aaae4f55c
drshariqalikhan <speromedtech@gmail.com>
2019-02-15 12:35:16 +0800

    o

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+import requests
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-   return "hello"
+   code = requests.get("https://google.com")
+   return "hi %s" %code.status_code
