eae15653d4ff67c0aa260e771b087abe16cfa19e
drshariqalikhan <speromedtech@gmail.com>
2019-02-15 13:48:30 +0800

    o

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ app = Flask(__name__)
 def index():
    r = requests.get("https://google.com")
    s = BeautifulSoup(r.content,'lxml')
-   return "hi there %s" %r.status_code
+   return "hi there bro %s" %r.status_code
