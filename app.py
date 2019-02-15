e9c8c386960ee0be0d311259f3c1349ba1c56647
drshariqalikhan <speromedtech@gmail.com>
2019-02-15 12:46:53 +0800

    o

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from bs4 import BeautifulSoup as bs
+# from bs4 import BeautifulSoup as bs
 import requests
 app = Flask(__name__)
 
 @app.route('/')
 def index():
    r = requests.get("https://google.com")
-   s = bs(r,'lxml')
+   # s = bs(r,'lxml')
    return "hi %s" %r.status_code
