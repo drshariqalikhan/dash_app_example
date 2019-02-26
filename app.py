ae6b6a99c7688daae48f4f5ea5f372166982fdb8
drshariqalikhan <speromedtech@gmail.com>
2019-02-26 16:17:41 +0800

    o

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import requests
 import os
 import json
 from flask_sqlalchemy import SQLAlchemy
-from flask.ext.heroku import Heroku
+
 
 # from files.parser import parse_page
 # from files.search import searchFor
@@ -17,9 +17,11 @@ app = Flask(__name__)
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://oipfsfiayphqsu:b9d7b026fc00203e471d8c9020284a2fd6285516ef8f50230dfef22379a7f374@ec2-50-17-193-83.compute-1.amazonaws.com:5432/d5lvt5aov2s0ou'
 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+
 # app.config.from_object(os.environ['APP_SETTINGS'])
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-heroku = Heroku(app)
+# heroku = Heroku(app)
 db = SQLAlchemy(app)
 
 @app.route('/')
