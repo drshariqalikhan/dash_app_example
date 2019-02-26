49f8d7a801e12618d9b3cdd503400438ea07bd38
drshariqalikhan <speromedtech@gmail.com>
2019-02-26 16:39:15 +0800

    o

--- app.py
+++ app.py
@@ -12,16 +12,11 @@ from models import User
 # from files.geo import getCity
 #DATABASE_URL: postgres://oipfsfiayphqsu:b9d7b026fc00203e471d8c9020284a2fd6285516ef8f50230dfef22379a7f374@ec2-50-17-193-83.compute-1.amazonaws.com:5432/d5lvt5aov2s0ou
 
-
 app = Flask(__name__)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://oipfsfiayphqsu:b9d7b026fc00203e471d8c9020284a2fd6285516ef8f50230dfef22379a7f374@ec2-50-17-193-83.compute-1.amazonaws.com:5432/d5lvt5aov2s0ou'
 DATABASE_DEFAULT = 'postgresql://postgres:14051976@localhost/flaskmovie'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-
-# app.config.from_object(os.environ['APP_SETTINGS'])
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# heroku = Heroku(app)
+app.debug = True
 db = SQLAlchemy(app)
 
 @app.route('/')
