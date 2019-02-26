92a8495eed3a5b948b6741c1a15cc3b53a8e0f6c
drshariqalikhan <speromedtech@gmail.com>
2019-02-26 14:11:09 +0800

    o

---
+++ db_create.py
@@ -0,0 +1,4 @@
+from app import db
+from models import FluDb
+
+db.create_all()
---
+++ models.py
@@ -0,0 +1,14 @@
+from app import db
+from sqlalchemy.dialects.postgresql import JSON
+
+class FluDb(db.metadata):
+    __tablename__ = "News"
+
+    id = db.Column(db.integer,primary_key = True)
+    news = db.Column(JSON)
+
+    def __init__(self,news):
+        self.news = news
+    
+    def __repr__(self):
+        return '<news %r>' %self.news
