1c3add41f093d6f6a48130683f1fc3c1abe8d69c
drshariqalikhan <speromedtech@gmail.com>
2019-02-26 16:48:32 +0800

    o

--- app.py
+++ app.py
@@ -21,12 +21,12 @@ db = SQLAlchemy(app)
 class User(db.Model):
 
    id = db.Column(db.Integer,primary_key = True)
-   # username = db.Column(db.String(80),unique = True)
+   username = db.Column(db.String(80),unique = True)
    email = db.Column(db.String(120),unique = True)
 
    def __init__(self,email):
-      
-      # self.username = username
+
+      self.username = username
       self.email = email
 
    def __repr__(self):
